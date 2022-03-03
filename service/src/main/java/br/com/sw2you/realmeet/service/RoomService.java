8c520343459f1187342151e545c6c5290fd13ce0
FernandoVictorff <fernandovictor01@gmail.com>
2022-03-03 15:25:46 -0300

    Criacao dos testes RoomMapperUnitTest e RoomServiceUnitTest e modificando alguns arquivos

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -6,6 +6,7 @@ import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +15,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final RoomMapper roomMapper;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, RoomMapper roomMapper) {
         this.roomRepository = roomRepository;
+        this.roomMapper = roomMapper;
     }
 
     @GetMapping
     public RoomDTO getRoom(@RequestBody Long id) {
         requireNonNull(id);
-        Room room = roomRepository.findById(id).orElseThrow(RoomNotFoundException::new);
-        return new RoomDTO().name(room.getName()).id(room.getId()).seats(room.getSeats());
+        Room room = roomRepository.findById(id).orElseThrow(() -> new RoomNotFoundException("Room not found: " + id));
+        return roomMapper.fromEntityToDto(room);
     }
 
 }
